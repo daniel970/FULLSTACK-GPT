b41bcba132ee02f7aeee1caa0da97abce0df9298
DYK <doyoung970@gmail.com>
2024-09-05 11:01:16 +0900

    added chain and llm function

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -5,13 +5,19 @@ from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
 from langchain.storage import LocalFileStore
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
+from langchain.prompts import ChatPromptTemplate
 
 st.set_page_config(
     page_title="DocumentGPT Home",
     page_icon="🤖",
+)
+
+llm = ChatOpenAI(
+    temperature=0.1,
 )
 
 @st.cache_data(show_spinner="Embedding file...")
@@ -50,7 +56,8 @@ def send_message(message, role, save=True):
     if save:
         st.session_state["messages"].append({"message":message, "role":role})
 
-
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
 
 def paint_history():
     for message in st.session_state["messages"]:
@@ -60,16 +67,28 @@ def paint_history():
             save=False,
         )
 
+prompt = ChatPromptTemplate.from_messages([
+    ("system", 
+        """
+        Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T ,make anything up.
+            
+        Context: {context}
+        """,
+    ),
+    ("human" "{question}"),
+])
 
 st.title("DocumentGPT")
 
-st.markdown("""
-            Welcome!
+st.markdown(
+    """
+    Welcome!
 
-Use this chatbot to ask questions to an AI about your files!
+    Use this chatbot to ask questions to an AI about your files!
             
-Upload your files on the sidebar.
-""")
+    Upload your files on the sidebar.
+    """
+)
 
 with st.sidebar:
     file = st.file_uploader(
@@ -86,6 +105,13 @@ if file:
 
     if message:
         send_message(message, "human")
-    
+        chain = {
+            "context": retriever | RunnableLambda(format_docs),
+            "question": RunnablePassthrough()
+        } | prompt | llm
+        
+        response = chain.invoke(message)
+        send_message(response.content, "ai")
+
 else:
     st.session_state["messages"] = []
