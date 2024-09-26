a448dcea97cb093168d7aeae273a40feb05813d6
DYK <doyoung970@gmail.com>
2024-09-26 15:33:45 +0900

    applied gpt-3.5-turbo-0125

--- pages/02_QuizGPT.py
+++ pages/02_QuizGPT.py
@@ -2,6 +2,7 @@ from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import CharacterTextSplitter
 import streamlit as st
 from langchain.retrievers import WikipediaRetriever
+from langchain.chat_models import ChatOpenAI
 
 
 st.set_page_config(
@@ -11,6 +12,10 @@ st.set_page_config(
 
 st.title("QuizGPT")
 
+llm = ChatOpenAI(
+    temperature=0.1,
+    model="gpt-3.5-turbo-0125",
+)
 
 @st.cache_data(show_spinner="Loading file...")
 def split_file(file):
@@ -29,6 +34,7 @@ def split_file(file):
 
 
 with st.sidebar:
+    docs = None
     choice = st.selectbox(
         "Choose what you want to use.",
         (
@@ -50,3 +56,12 @@ with st.sidebar:
             retriever = WikipediaRetriever(top_k_results=5)
             with st.status("Searching Wikipedia..."):
                 docs = retriever.get_relevant_documents(topic)
+
+
+if not docs:
+    st.markdown("""
+
+                """)
+    
+else:
+    st.write(docs)
