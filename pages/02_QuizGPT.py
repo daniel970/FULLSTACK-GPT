3ffa030d8a7f85b1278eb0825dca690fbd9a9794
DYK <doyoung970@gmail.com>
2024-09-26 14:45:48 +0900

    added search function

--- pages/02_QuizGPT.py
+++ pages/02_QuizGPT.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from langchain.retrievers import WikipediaRetriever
 
 st.set_page_config(
     page_icon="❓",
@@ -6,3 +7,20 @@ st.set_page_config(
 )
 
 st.title("QuizGPT")
+
+with st.sidebar:
+    choice = st.selectbox("Choose what you want to use.",
+                          (
+                              "File",
+                              "Wikipedia Article",
+                          ),
+                          )
+    if choice == "File":
+        file = st.file_uploader("Upload a .docx, .txt or .pdf file", type=["pdf", "txt", "docx"],
+                                )
+    else:
+        topic = st.text_input("Search Wikipedia...")
+        if topic:
+            retriever = WikipediaRetriever(top_k_results=1)
+            with st.status("Searching Wikipedia..."):
+                docs = retriever.get_relevant_documents(topic)
